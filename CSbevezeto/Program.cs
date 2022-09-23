bc7542f0d5063afa487492b474a41029c6d41b8e
Gábor Garai <garai.gabor@petrik.hu>
2022-09-23 19:59:19 +0200

    Add project files.

---
+++ CSbevezeto/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CSbevezto
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+
