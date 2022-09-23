33eb2293e5c22503d3c3a3286a1e0d4db318c8a4
Gábor Garai <garai.gabor@petrik.hu>
2022-09-23 20:22:51 +0200

    1. feladat

--- CSbevezeto/Program.cs
+++ CSbevezeto/Program.cs
@@ -4,7 +4,20 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Feladat_1();
+        }
+
+        private static void Feladat_1()
+        {
+            Console.Write("Adja meg a medence átmérőjét (m): ");
+            double atmero = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Adja meg a medence mélységét (m): ");
+            double mely = Convert.ToDouble(Console.ReadLine());
+            double sugar = atmero / 2;
+            double alapTerulet = sugar * sugar * Math.PI;
+            double terfogat = alapTerulet * mely;
+
+            Console.WriteLine("A medencébe " + Math.Round(terfogat,2) + " köbméter víz fér.");
         }
     }
 }
